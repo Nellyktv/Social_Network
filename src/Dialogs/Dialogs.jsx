8997525f43048a0b250f22e3dc1b426e246a97ec
Dmitriy <khotindima@gmail.com>
2023-09-22 22:59:52 +0300

    Added new changes

--- src/Dialogs/Dialogs.jsx
+++ src/Dialogs/Dialogs.jsx
@@ -2,25 +2,40 @@ import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css';
 import React from 'react';
 
+
+const DialogItem = (props) => {
+    return (
+        <>
+            <div className={styles.item}>
+                <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+            </div>
+        </>
+    )
+
+}
+
+
+const Message = (props) =>{
+    return(
+        <>
+    <div className={styles.messages}>
+    <div className={styles.message}>{props.message}</div>
+    </div>
+    </>
+)
+}
+
 export default function Dialogs(props) {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogs_items}>
-                <div className={styles.item}>
-                    <NavLink to='/dialogs/1'>Ира</NavLink>
-                    </div>
-                <div className={styles.item}>
-                <NavLink to='/dialogs/2'>Настя</NavLink>
-                    </div>
-                <div className={styles.item}>
-                <NavLink to='/dialogs/3'>Оля</NavLink>
-                    </div>         
+                <DialogItem name='Ира' id='1' />
+                <DialogItem name='Настя' id='2' />
+                <DialogItem name='Оля' id='3' />
             </div>
-            <div className={styles.messages}>
-            <div className={styles.message}>Hi</div>
-            <div className={styles.message}>How is your it-kamasutra?</div>
-            <div className={styles.message}>Yo</div>
-            </div>
+            <Message message='Hi'/>
+            <Message message='How is your it-kamasutra?'/>
+            <Message message='Yo'/>
         </div>
     );
 }
